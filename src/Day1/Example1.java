e6f6d8ffae0b46c7036fda6ee83223bb453084fa
Yamini Nagamani <yamini.nagamani@yexledomain.com>
2024-06-25 17:27:22 +0530

    Sample commit

---
+++ src/Day1/Example1.java
@@ -0,0 +1,15 @@
+package Day1;
+
+import org.testng.annotations.Test;
+
+public class Example1 {
+
+	@Test
+	public void Demo()
+	{
+	
+System.out.println("hello");
+
+	}
+
+}
---
+++ src/day2/hello.java
@@ -0,0 +1,16 @@
+package day2;
+
+import org.testng.annotations.Test;
+
+public class hello {
+
+	@Test
+	public void Demo()
+	{
+	
+System.out.println("hello");
+
+	}
+
+}
+
